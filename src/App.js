31e4110e5201d730cb3a0b6de34d1702fd2b5734
JO81N <jobinj2512@gmail.com>
2023-12-08 13:25:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Home from './Pages/Home/Home';
+import SignUp from './Pages/SignUp/SignUp';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './App.css'
+import Login from './Pages/Login/Login';
+import Lock from './Pages/Lock/Lock';
+import Teaser from './Pages/Trailer/Teaser';
+import Poster from './Store/PosterContext';
+import BannerTrailer from './Pages/Trailer/BannerTrailer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Poster>
+    <Router>
+      <Routes>
+        <Route path='/' element={<SignUp/>}/>
+        <Route path='/Netflix.com' element={<SignUp/>}/>
+        <Route path='/signup' element={<Login/>}/>
+        <Route path='/netflix.com' element={<Home/>}/>
+        <Route path='/signin' element={<Lock/>}/>
+        <Route path='/teaser' element={<Teaser/>}/>
+        <Route path='/Bteaser' element={<BannerTrailer/>}/>
+      </Routes>
+    </Router>
+    </Poster>
     </div>
   );
 }
